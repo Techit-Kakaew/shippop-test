7f3bbc8396f5dd70110e3c79248984b339a87587
Techit-Kakaew <techit.kakaew@gmail.com>
2021-11-19 13:29:20 +0700

    initial project

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <Home />
+</template>
+
+<script>
+import Home from '@/components/Home.vue'
+export default {
+  name: 'HOME',
+  components: {
+    Home
+  },
+  setup() {
+    
+  },
+}
+</script>
